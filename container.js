8e8dbd79945838545671ee83eb843f908bf828ff
yasha <yashakhan4761@gmail.com>
2023-04-09 16:19:13 +0530

    webtask

---
+++ container.js
@@ -0,0 +1,4 @@
+var CON= ["images/c2.png","images/c3.png","images/c4.png","images/c5.png","images/c6.png","images/c7.png","images/c8.png","images/c9.png"
+        ,"images/c10.png","images/d1.png","images/d2.png","images/d3.png","images/d4.png","images/d5.png","images/d6.png","images/d7.png",
+        "images/d8.png","images/d9.png","images/d10.png","images/d11.png","images/e1.png","images/e2.png","images/e3.png","images/e4.png",
+        "images/e5.png","images/e6.png","images/e7.png","images/e8.png","images/e9.png",]
